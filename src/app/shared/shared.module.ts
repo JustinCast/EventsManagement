5483b7e1f94d553ce8cb523e51ac412b8b376e54
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-09-11 23:55:12 -0600

    feat: esqueleto y configuración del proyecto

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { 
+  
+ } from "@angular/material";
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class SharedModule { }
