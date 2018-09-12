8c113f306c9a4ca7232dcbac5f0dd06b1f99de87
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-09-12 00:09:18 -0600

    feat: cambios importantes en el proyecto

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { 
-  
+  MatInputModule
  } from "@angular/material";
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatInputModule
+  ],
+  exports: [
+    MatInputModule
   ],
   declarations: []
 })
