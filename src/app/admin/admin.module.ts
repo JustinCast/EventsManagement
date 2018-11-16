a42d2fc5072b594b684fdf41997b33efdcb85279
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-11-15 19:56:03 -0600

    feat: addevents components

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { AddActivityComponent } from './Activity/add-activity/add-activity.component';
 import { AdminRouting } from './admin.routing';
 import { ShowActivitiesComponent } from './Activity/show-activities/show-activities.component';
+import { AddEventComponent } from './Event/add-event/add-event.component';
+import { ShowEventsComponent } from './Event/show-events/show-events.component';
 
 @NgModule({
-  declarations: [AddActivityComponent, ShowActivitiesComponent],
+  declarations: [AddActivityComponent, ShowActivitiesComponent, AddEventComponent, ShowEventsComponent],
   imports: [
     CommonModule,
     AdminRouting
--- src/app/admin/admin.routing.ts
+++ src/app/admin/admin.routing.ts
@@ -4,12 +4,16 @@ import { NgModule } from "@angular/core";
 import { AdminRootComponent } from "./admin-root.component";
 import { AddActivityComponent } from "./Activity/add-activity/add-activity.component";
 import { ShowActivitiesComponent } from "./Activity/show-activities/show-activities.component";
+import { AddEventComponent } from "./Event/add-event/add-event.component";
+import { ShowEventsComponent } from "./Event/show-events/show-events.component";
 
 const ROUTES: Routes = [
   {
     path: "admin",
     component: AdminRootComponent,
     children: [
+      { path: "add-event", component: AddEventComponent },
+      { path: "show-events", component: ShowEventsComponent },
       { path: "add-activity", component: AddActivityComponent },
       { path: "show-activities", component: ShowActivitiesComponent },
     ]
