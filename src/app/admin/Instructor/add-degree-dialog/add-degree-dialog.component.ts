2aa584665777322d4b12328c574b0b0d7bac4dd4
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-11-16 03:16:51 -0600

    feat: last changes

---
+++ src/app/admin/Instructor/add-degree-dialog/add-degree-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-degree-dialog',
+  templateUrl: './add-degree-dialog.component.html',
+  styleUrls: ['./add-degree-dialog.component.scss']
+})
+export class AddDegreeDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
