d7969716ec20d710162f708ffbd3f92a4cf7b539
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-11-18 22:43:33 -0600

    feat: email working partially

--- src/app/admin/reminder/reminder.component.ts
+++ src/app/admin/reminder/reminder.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import * as emailjs from "emailjs-com";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 @Component({
-  selector: 'app-reminder',
-  templateUrl: './reminder.component.html',
-  styleUrls: ['./reminder.component.scss']
+  selector: "app-reminder",
+  templateUrl: "./reminder.component.html",
+  styleUrls: ["./reminder.component.scss"]
 })
 export class ReminderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  emailGroup: FormGroup;
+  constructor(private _fb: FormBuilder) {
+    this.emailGroup = this._fb.group({
+      'to_name': ['', Validators.required],
+      'from_name': ['', Validators.required],
+      'message_html': ['', Validators.required],
+    })
   }
 
+  ngOnInit() {
+    emailjs.init("user_RjLHFaNOVjh9MHP1hlwz9");
+  }
+
+  sendEmail() {
+    let templateParams = {
+      to_name: this.emailGroup.get('to_name').value,
+      message_html: this.emailGroup.get('message_html').value,
+      from_name: this.emailGroup.get('from_name').value
+  };
+    emailjs
+      .send(
+        "gmail",
+        "template_weEmF6VN",
+        JSON.stringify(templateParams),
+        "user_RjLHFaNOVjh9MHP1hlwz9"
+      )
+      .then(
+        response => {
+          console.log("SUCCESS!", response.status, response.text);
+        },
+        err => {
+          console.log("FAILED...", err);
+        }
+      );
+  }
 }
